6e76241f7afc0a2bef2d24fc8cc494b41058927b
Derik <62659313+makemepresident@users.noreply.github.com>
2022-05-03 21:41:14 -0700

    push for remote work

--- src/typey/filecontroller.py
+++ src/typey/filecontroller.py
@@ -5,11 +5,11 @@ from types import SimpleNamespace
 
 class FileController:
 
-    themes_path = pkg_resources.resource_filename("typey", "./assets/themes.json")
-    defaults_path = pkg_resources.resource_filename("typey", "./assets/defaults.json")
-    words_path = pkg_resources.resource_filename("typey", "./assets/1-1000.txt")
-    themes_path = './src/typey/assets/config.json'
-    defaults_path = './src/typey/assets/defaults.json'
+    # themes_path = pkg_resources.resource_filename("typey", "./assets/themes.json")
+    # defaults_path = pkg_resources.resource_filename("typey", "./assets/defaults.json")
+    # words_path = pkg_resources.resource_filename("typey", "./assets/1-1000.txt")
+    themes_path = './src/typey/assets/themes.json'
+    defaults_path = './src/typey/assets/config.json'
     words_path = './src/typey/assets/1-1000.txt'
 
     @staticmethod
